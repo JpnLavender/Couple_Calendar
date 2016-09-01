9df1299e87de39f0e0db6fe6b1d01bdf4411a514
JpnLavender <nagisa20000014@gmail.com>
2016-09-01 12:46:49 +0900

    [Add] app.rb < sign_in System & sign_up System

--- app.rb
+++ app.rb
@@ -13,7 +13,26 @@ get '/sign_in' do
   erb :sign
 end
 
+post '/sign_in' do
+  user = User.find_by_mail(params[:mail])
+  if user && user.authenticate(params[:password])
+    session[:user] = user.user_token
+    redirect '/'
+  else
+    redirect '/sign_in'
+  end
+end
+
 get '/sign_up' do
   @sign_action, @sign_submit_text = "/sign_up", "CreateAccount"
   erb :sign
+end
+
+post '/sign_up' do
+  unless User.where(user_name: params[:user_name]).exists?
+    User.create(mail: params[:mail], user_token: Create.token ,password: params[:password], password_confirmation: params[:password])
+    redirect '/'
+  else
+    redirect '/sign_up'
+  end
 end
